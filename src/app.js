a700805a34c65a3f43a10a6fbfa264e1603b8137
Milena Concha <milena.concha.m@gmail.com>
2024-12-08 19:55:32 +0000

    dominios listos

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dominio = [".com", ".net", ".cl", ".es"];
+  let resultado = "";
+  for (let indexpronoun = 0; indexpronoun < pronoun.length; indexpronoun++) {
+    for (let indexadj = 0; indexadj < adj.length; indexadj++) {
+      for (let indexnoun = 0; indexnoun < noun.length; indexnoun++) {
+        for (
+          let indexdominio = 0;
+          indexdominio < dominio.length;
+          indexdominio++
+        ) {
+          resultado =
+            resultado +
+            pronoun[indexpronoun] +
+            adj[indexadj] +
+            noun[indexnoun] +
+            dominio[indexdominio] +
+            "\n";
+        }
+      }
+    }
+  }
+  console.log(resultado);
+  let elemento = document.getElementById("dominios");
+  elemento.innerText = resultado;
 };
